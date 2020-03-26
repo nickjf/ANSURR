b6d88d3134f4851d88aca92d4981a9263d092ebd
nickjf <nickjfowler0@gmail.com>
2020-03-26 09:09:40 +0000

    ANSURR v0.4 updated extract algorithm to deal with sudden change in chain

--- progs/extract.py
+++ progs/extract.py
@@ -29,11 +29,10 @@ else:
 
 standard_res = ['ALA','CYS','ASP','GLU','PHE','GLY','HIS','ILE','LYS','LEU','MET','ASN','PRO','GLN','ARG','SER','THR','VAL','TRP','TYR']
 
-pdb_lines = []
+pdb_lines = {}
 models = {}
 model = '1' # assume first model is model 1
 chain = ''
-prev_chain = ''
 nonstandard = []
 
 for line in pdb_in:
@@ -44,53 +43,65 @@ for line in pdb_in:
 	elif line[:5] == 'MODEL':
 		model = line.split()[1]
 	elif line[:4] == 'ATOM' or line[:6] == 'HETATM':
-		pdb_lines.append(line)
-		chain = '' if chain == ' ' else line[21]
+		chain = '' if line[21] == ' ' else line[21]
+		if chain not in pdb_lines:
+			pdb_lines[chain] = [line]
+		else:
+			pdb_lines[chain].append(line)
 	elif 'TER' in line[:3] or 'END' in line[:3]:
 		if len(pdb_lines) > 0:   
-			selected_pdb_lines = []
-			std_res = 0
-			for line in pdb_lines:
-				resn = line[17:20]
-				if resn in standard_res and line[:4] == 'ATOM':
-					std_res = 1
-					break
-			if std_res == 1:
-				for line in pdb_lines:
+			selected_pdb_lines = {}
+			for chain in pdb_lines:
+				std_res = 0
+				for line in pdb_lines[chain]:
 					resn = line[17:20]
-					if resn in standard_res:
-						selected_pdb_lines.append(line)
-					elif nonstandardres == 1:
-						selected_pdb_lines.append('ATOM  '+line[6:])
-						if resn not in nonstandard:
-							print(" -> non-standard residue "+resn.replace(' ','')+" will be included in rigidity calculations")
-							nonstandard.append(resn)
-					else:
-						if resn not in nonstandard:
-							print(" -> found a non-standard residue ("+resn.replace(' ','')+") which are ignored by default. To include non-standard residues, re-run ANSURR with the -n flag")
-							nonstandard.append(resn)
-			else:
-				for line in pdb_lines:
-					resn = line[17:20]
-					if freeligands == 1:
-						selected_pdb_lines.append(line)
-						if resn not in nonstandard:
-							print(" -> free ligand "+resn.replace(' ','')+" will be included in rigidity calculations")
-							nonstandard.append(resn)
-					else:
-						if resn not in nonstandard:
-							print(" -> found a free ligand ("+resn.replace(' ','')+") which are ignored by default. To include free ligands, re-run ANSURR with the -l flag")
-							nonstandard.append(resn)
-			
-			if len(selected_pdb_lines) > 0:
-				if model not in models:  
-					models[model] = {chain:selected_pdb_lines}
-				elif chain not in models[model]:
-					models[model][chain] = selected_pdb_lines
+					if resn in standard_res and line[:4] == 'ATOM':
+						std_res = 1
+						break
+				if std_res == 1:
+					for line in pdb_lines[chain]:
+						resn = line[17:20]
+						if resn in standard_res:
+							if chain not in selected_pdb_lines:
+								selected_pdb_lines[chain] = [line]
+							else:
+								selected_pdb_lines[chain].append(line)
+						elif nonstandardres == 1:
+							if chain not in selected_pdb_lines:
+								selected_pdb_lines[chain] = ['ATOM  '+line[6:]]
+							else:
+								selected_pdb_lines[chain].append('ATOM  '+line[6:])
+							if resn not in nonstandard:
+								print(" -> non-standard residue "+resn.replace(' ','')+" will be included in rigidity calculations")
+								nonstandard.append(resn)
+						else:
+							if resn not in nonstandard:
+								print(" -> found a non-standard residue ("+resn.replace(' ','')+") which are ignored by default. To include non-standard residues, re-run ANSURR with the -n flag")
+								nonstandard.append(resn)
 				else:
-					models[model][chain].extend(selected_pdb_lines)
-			pdb_lines = []
-	prev_chain = chain
+					for line in pdb_lines[chain]:
+						resn = line[17:20]
+						if freeligands == 1:
+							if chain not in selected_pdb_lines:
+								selected_pdb_lines[chain] = [line]
+							else:
+								selected_pdb_lines[chain].append(line)
+							if resn not in nonstandard:
+								print(" -> free ligand "+resn.replace(' ','')+" will be included in rigidity calculations")
+								nonstandard.append(resn)
+						else:
+							if resn not in nonstandard:
+								print(" -> found a free ligand ("+resn.replace(' ','')+") which are ignored by default. To include free ligands, re-run ANSURR with the -l flag")
+								nonstandard.append(resn)
+			for chain in selected_pdb_lines:
+				if len(selected_pdb_lines[chain]) > 0:
+					if model not in models:  
+						models[model] = {chain:selected_pdb_lines[chain]}
+					elif chain not in models[model]:
+						models[model][chain] = selected_pdb_lines[chain]
+					else:
+						models[model][chain].extend(selected_pdb_lines[chain])
+			pdb_lines = {}
 
 chains_done = []
 resi_ref = {}
