ANSURR v0.8 average compare RC fix

progs/compare_average.py
@@ -77,7 +77,7 @@ for chain_shiftID in av:
     resi = [int(i) for i in av[chain_shiftID]]
     RCI = [av[chain_shiftID][i]['rci'] for i in av[chain_shiftID]]
     FIRST = [av[chain_shiftID][i]['first'] for i in av[chain_shiftID]]
-    shifts = [av[chain_shiftID][i]['shifts'] for i in av[chain_shiftID]]
+    shifts = [av[chain_shiftID][i]['shift_perc'] for i in av[chain_shiftID]]
 
     if av_perc_shifts < 75:
         av_perc_shifts_out = str(av_perc_shifts)+' (RCI may be unreliable!)'
@@ -86,10 +86,14 @@ for chain_shiftID in av:
         av_perc_shifts_out = str(av_perc_shifts)
         #print('DONE')
 
+
     RCI_nan = [i for i,x in enumerate(RCI) if np.isnan(x)]
     FIRST_nan = [i for i,x in enumerate(FIRST) if np.isnan(x)]
-    RCI_noRC = [x for i,x in enumerate(RCI) if i not in FIRST_nan and not np.isnan(x) and shifts[i] != 'RC']
-    FIRST_noRC = [x for i,x in enumerate(FIRST) if i not in RCI_nan and not np.isnan(x) and shifts[i] != 'RC']
+    RCI_noRC = [x for i,x in enumerate(RCI) if i not in FIRST_nan and not np.isnan(x) and shifts[i] >=0.17 ]
+    FIRST_noRC = [x for i,x in enumerate(FIRST) if i not in RCI_nan and not np.isnan(x) and shifts[i] >=0.17]
+
+
+    print(FIRST_noRC)
 
     RMSD_noRC =  calc_RMSD(RCI_noRC,FIRST_noRC)
     RMSD_score = 100.0 - percentileofscore(rmsd_benchmark,RMSD_noRC)
@@ -110,7 +114,7 @@ for chain_shiftID in av:
 
     # append to scores.out
     scores = open('scores_average.out','a+')
-    scores.write('PDB: '+ '{:<11}'.format(PDB_ID+chain_shiftID.split('_')[0]) + ' SHIFTS: '+ '{:<11}'.format(SHIFT_ID+chain_shiftID.split('_')[1]) + ' SHIFT%: '+ '{:3}'.format(av_perc_shifts) + ' Spearman: '+ '{: 5.2f}'.format(spearman_noRC) + ' CorrelationScore: '+ '{:4.1f}'.format(corr_score) + ' RMSD: '+ '{:4.2f}'.format(RMSD_noRC) + ' RMSDScore: '+ '{:4.1f}'.format(RMSD_score)+ '\n')
+    scores.write('PDB: '+ '{:<11}'.format(PDB_ID+chain_shiftID.split('_')[0]) + ' SHIFTS: '+ '{:<11}'.format(SHIFT_ID+'_'+chain_shiftID.split('_')[1]) + ' SHIFT%: '+ '{:3}'.format(av_perc_shifts) + ' Spearman: '+ '{: 5.2f}'.format(spearman_noRC) + ' CorrelationScore: '+ '{:4.1f}'.format(corr_score) + ' RMSD: '+ '{:4.2f}'.format(RMSD_noRC) + ' RMSDScore: '+ '{:4.1f}'.format(RMSD_score)+ '\n')
     scores.close()
     plot(resi,RCI,FIRST,n)
     n+=1                                                                  