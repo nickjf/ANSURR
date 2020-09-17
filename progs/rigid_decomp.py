028e79a433702894260d97f52c30256fac4e2891
nickjf <nickjfowler0@gmail.com>
2020-09-17 11:46:14 +0100

    ANSURR v0.9 important fix to rigid_decomp.py

--- progs/rigid_decomp.py
+++ progs/rigid_decomp.py
@@ -1,5 +1,4 @@
 # args pdb decomp_list
-
 import sys, os
 from collections import Counter
 
@@ -12,17 +11,15 @@ class res(object):
         self._registry.append(self)
         self.i = i
         self.name = name
-        self.atoms = [atom]
+        self.CA = atom
         self.clusters = []
         self.energy = 0
 
 # get atom numbers for each CA atom from pdb
-atom_number = 0
 for line in pdb:
     if line[:4] == 'ATOM' or line[:6] == 'HETATM':
         atom_number = int(line[6:11])
-        #atom_number += 1
-        atom_name = line[12:16]#.replace(' ','')
+        atom_name = line[12:16]
         resn = line[17:20].replace(' ','')
         resi = int(line[22:26])
         if atom_name == ' CA ':
@@ -34,35 +31,34 @@ all_data = []
 energy = []
 rigid_clusters = []
 for line in decomp:
-    if 'END' in line:
-        rc = []
-        data = data[:atom_number] # remove overflow
-        all_data.append(data)
-        cluster_count = Counter(data)
-        for c in cluster_count:
-            if int(cluster_count[c]) >= 15:
-                rc.append(c)
-        rigid_clusters.append(rc)
-        data = []
-    elif 'A:' in line:
-        energy.append(float(line[10:21]))
-    elif 'A' not in line and 'B' not in line:
-        line = line.replace('\n','').split(':')
-        for l in line:
-            data.append(int(l))  
+    if 'HEADER' not in line[:6]:
+        if 'END' in line[:3]:
+            rc = []
+            data = data[:atom_number] # remove overflow
+            all_data.append(data)
+            cluster_count = Counter(data)
+            for c in cluster_count:
+                if int(cluster_count[c]) >= 15:
+                    rc.append(c)
+            rigid_clusters.append(rc)
+            data = []
+        elif 'A:' in line[:2]:
+            energy.append(float(line[10:21]))
+        elif 'A:' not in line[:2] and 'B:' not in line[:2]:
+            line = line.replace('\n','').split(':')
+            for l in line:
+                data.append(int(l))  
 
 # append rigid clusters to residues
 for d in all_data:
-    for r in res._registry:
-        r.clusters.append([d[a] for a in r.atoms])
+     for r in res._registry:
+        r.clusters.append(d[r.CA-1])
 
 # find rigid clusters
 for x in range(len(all_data)):
     for r in res._registry:
-        cluster = [y for y in r.clusters[x] if y != 0]
-        for c in cluster:
-            if c in rigid_clusters[x]:
-                r.energy = energy[x]
+        if r.clusters[x] in rigid_clusters[x]:
+            r.energy = energy[x]
 
 #write output
 out = open(os.path.basename(sys.argv[1]).split('.')[0]+'.decomp','w')
